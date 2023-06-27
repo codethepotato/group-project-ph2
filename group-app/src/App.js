8a025a11bbf5d06e793fbb22512f9349d73ac87c
Erica Belzer <belzer49@gmail.com>
2023-06-27 11:19:58 -0500

    clean up

--- group-app/src/App.js
+++ group-app/src/App.js
@@ -1,8 +1,17 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './Components/Header';
 
 function App() {
+
+  // const [allPokemons, setAllPokemons] = useState([])
+
+  // useEffect(() => {
+  //   fetch('http://localhost:3001/pokemon')
+  //     .then((r) => r.json())
+  //     .then((allPokemons) => console.log(allPokemons))
+  // }, [])
+
   return (
     <div className="App">
       <Header />
--- group-app/src/Components/Header.js
+++ group-app/src/Components/Header.js
@@ -1,18 +1,16 @@
+// import logo from './logo.svg';
+
 function Header() {
     return (
         <div id='header'>
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
+                <img src={'logo'} className="App-logo" alt="Get wrecked" />
+                <p>Howdy Y'all</p>
                 <a
                     className="App-link"
                     href="https://reactjs.org"
                     target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
+                    rel="noopener noreferrer">
                 </a>
             </header>
         </div>

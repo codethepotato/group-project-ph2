33e50e839055e181158a89d0a22ee16299477de0
Adam Mauch <maucap20@wfu.edu>
2023-06-27 12:25:11 -0400

    style stuff

--- group-app/src/Components/Header.js
+++ group-app/src/Components/Header.js
@@ -2,18 +2,9 @@ function Header() {
     return (
         <div id='header'>
             <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>src/App.js</code> and save to reload.
+                   POKEMANS
                 </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
             </header>
         </div>
     )
